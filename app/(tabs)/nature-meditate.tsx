fc1a5724a4be6b125994df8c228bdbe12591bd14
yogeshprakhar <yogeshprakharpal200027@gmail.com>
2024-08-04 22:51:36 +0530

    change name

--- app/(tabs)/nature-meditate.tsx
+++ app/(tabs)/nature-meditate.tsx
@@ -15,7 +15,7 @@ const Meditate = () => {
       <AppGradient colors={["#161b2e", "#0a4d4a", "#766e67"]}>
         <View className="mb-6">
           <Text className="text-gray-200 mb-3 font-bold text-4xl text-left">
-            Welcome Steven
+            Welcome Yogesh
           </Text>
           <Text className="text-indigo-100 text-l font-medium">
             Start your meditation practice
